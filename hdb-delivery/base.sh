cd553ed166a733e85776b21dce0ef457268a3389
jack <jack@jack-2.local>
2016-01-18 00:13:29 +0800

    add remote deploy script

---
+++ hdb-delivery/base.sh
@@ -0,0 +1,80 @@
+
+WORK_DIR=/data
+HTTP_BASIC_USER=hdb
+HTTP_BASIC_PASSPORD=p0o9i8u7
+TOMCAT_PACKAGE_NAME=apache-tomcat-7.0.65
+FILE_REPO=http://120.26.219.48:8080/
+
+DIR=$(cd "$(dirname "$0")";pwd)
+
+SSH_CMD=ssh
+SSH_OPTION="-o StrictHostKeyChecking=no -o PasswordAuthentication=no"
+SSH_KEY=${DIR}/hdb.key
+SSH_USER=root
+SCP_CMD=scp
+
+ssh_exec() {
+	if [ -f ${SSH_KEY} ]; then 
+		${SSH_CMD} ${SSH_OPTION} -i ${SSH_KEY} ${SSH_USER}@${1} $2
+	else 
+		${SSH_CMD} ${SSH_OPTION} ${SSH_USER}@${1} $2
+	fi
+	return $?
+}
+
+scp_exec() {
+	if [ -f ${SSH_KEY} ]; then 
+		${SCP_CMD} ${SSH_OPTION} -i ${SSH_KEY} -r ${2} ${SSH_USER}@${1}:"$3"
+	else 
+		${SCP_CMD} ${SSH_OPTION} -r ${2} ${SSH_USER}@${1}:"$3"
+	fi
+	return $?
+}
+
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+  exit 1
+}
+
+check_err() {
+  $1 2>&1  || err $2
+}
+
+
+check_port(){
+	nc -v -w 10 -z 127.0.0.1 $1
+	return $?
+}
+
+check_with_retry() {
+    cmd=$1
+    timeout=$2
+    for i in `seq 1 1 ${timeout}`; do
+        $1 && return 0
+        sleep 1
+    done
+    return 1
+}
+
+kill_process_with_sig() {
+	filter=${1}
+	sig=${2}
+	pids=`ps aux | grep ${filter} | grep -v grep | awk '{print $2}'`
+	[ -z ${pids} ] && return 0
+	kill -${sig}  $pids
+	sleep 1
+	pids=`ps aux | grep ${filter} | grep -v grep | awk '{print $2}'`
+	[ -z "${pids}" ] && return 0
+	return 1
+}
+
+kill_process() {
+	filter=$1
+	pids=`ps aux | grep ${filter} | grep -v grep | awk '{print $2}'`
+	[ -z ${pids} ] && return 0
+	kill $pids
+	sleep 1
+	pids=`ps aux | grep ${filter} | grep -v grep | awk '{print $2}'`
+	[ -z "${pids}" ] && return 0
+	return 1
+}
---
+++ hdb-delivery/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -el
+
+DIR=$(cd "$(dirname "$0")";pwd)
+
+. ${DIR}/base.sh
+
+usage() {
+	echo "$0 env componnet"
+}
+
+[ $# -ne 2 ] && usage && exit 1
+
+HDB_ENV=${1}
+COMPONNET=${2}
+
+[ ! -f ${DIR}/${COMPONNET}.rc ] && err "Componnet ${COMPONNET} not found!"
+
+. ${DIR}/${COMPONNET}.rc
+
+[ -z ${IPS} ] && err "Not target server found for Componnet ${COMPONNET}!"
+[ -z ${TOMCAT_SHUTDOWN_PORT} ] && err "Not TOMCAT_SHUTDOWN_PORT found for Componnet ${COMPONNET}!"
+[ -z ${TOMCAT_AJP_PORT} ] && err "Not TOMCAT_AJP_PORT found for Componnet ${COMPONNET}!"
+[ -z ${TOMCAT_LISTEN_PORT} ] && err "Not TOMCAT_LISTEN_PORT found for Componnet ${COMPONNET}!"
+
+IP_LIST=`echo ${IPS} | awk -F, '{NF-=0}1'`
+
+
+for ip in ${IP_LIST}; do
+	check_err "scp_exec ${ip} ${DIR}/base.sh /tmp/base.sh" "Failed to copy base.sh to ${ip}"
+	check_err "scp_exec ${ip} ${DIR}/delivery.sh /tmp/delivery.sh" "Failed to copy delivery.sh to ${ip}"
+	#check_err "ssh_exec ${ip} \'/bin/chmod +x /tmp/base.sh\'" "Failed to chmod +x base.sh to ${ip}"
+	#check_err "ssh_exec ${ip} \'/bin/chmod +x /tmp/delivery.sh\'" "Failed to chmod +x delivery.sh to ${ip}"
+	ssh_exec ${ip} "/bin/bash /tmp/delivery.sh ${HDB_ENV} ${COMPONNET} ${TOMCAT_LISTEN_PORT} ${TOMCAT_AJP_PORT} ${TOMCAT_SHUTDOWN_PORT} ${PORTS}"
+	[ ! $? ] && err "Failed to exec delivery.sh on ${ip}!"
+done
+exit 0
