42680b5cd98c1d5a2a35f67724fe6aab1e437bc8
itjunhua <huangjunhua@evergrande.com>
2016-01-29 17:59:32 +0800

    monitor ddl

---
+++ hdb-setup/ddl/monitor.sql
@@ -0,0 +1,46 @@
+/*
+SQLyog v10.2 
+MySQL - 5.6.27 : Database - dubbo_monitor
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dubbo_monitor` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `dubbo_monitor`;
+
+/*Table structure for table `dubbo_invoke` */
+
+DROP TABLE IF EXISTS `dubbo_invoke`;
+
+CREATE TABLE `dubbo_invoke` (
+  `id` varchar(255) NOT NULL DEFAULT '',
+  `invoke_date` date NOT NULL,
+  `service` varchar(255) DEFAULT NULL,
+  `method` varchar(255) DEFAULT NULL,
+  `consumer` varchar(255) DEFAULT NULL,
+  `provider` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT '',
+  `invoke_time` bigint(20) DEFAULT NULL,
+  `success` int(11) DEFAULT NULL,
+  `failure` int(11) DEFAULT NULL,
+  `elapsed` int(11) DEFAULT NULL,
+  `concurrent` int(11) DEFAULT NULL,
+  `max_elapsed` int(11) DEFAULT NULL,
+  `max_concurrent` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_service` (`service`),
+  KEY `idx_method` (`method`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
